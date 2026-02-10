b652e5ce7d9208a17a47df0e966d18ce56e41a74
Kamran Shahid <23-cs-77@students.uettaxila.edu.pk>
2026-02-10 17:34:18 +0500

    Time: 499 ms (81.72%), Space: 22.2 MB (57.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,22 +1,63 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         result = []
-        for i in range(len(nums)-2):
-            if i>0 and nums[i] == nums[i-1]:
+        n = len(nums)
+        
+        for i in range(n - 2):
+            # 1. Skip duplicates for the fixed number
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            l,r = i+1,len(nums)-1
+            
+            # 2. Optimization: If the smallest possible sum is > 0, stop everything
+            # (Because the list is sorted, all following numbers are even larger)
+            if nums[i] + nums[i+1] + nums[i+2] > 0:
+                break
+            
+            # 3. Optimization: If the largest possible sum for this 'i' is < 0, 
+            # this 'i' is too small, move to the next 'i'
+            if nums[i] + nums[n-2] + nums[n-1] < 0:
+                continue
+                
+            l, r = i + 1, n - 1
             while l < r:
-                three_sum = nums[l]+nums[r]+nums[i]
+                three_sum = nums[i] + nums[l] + nums[r]
+                
                 if three_sum == 0:
-                    result.append([nums[l],nums[r],nums[i]])
-                    l+=1
-                    r-=1
-                    while l<r and nums[l] == nums[l-1]:
-                        l+=1
+                    result.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    # 4. Skip duplicates for BOTH pointers to shrink window faster
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+                        
                 elif three_sum < 0:
-                    l+=1
+                    l += 1
                 else:
-                    r-=1
+                    r -= 1
+                    
         return result
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()
+#         result = []
+#         for i in range(len(nums)-2):
+#             if i>0 and nums[i] == nums[i-1]:
+#                 continue
+#             l,r = i+1,len(nums)-1
+#             while l < r:
+#                 three_sum = nums[l]+nums[r]+nums[i]
+#                 if three_sum == 0:
+#                     result.append([nums[l],nums[r],nums[i]])
+#                     l+=1
+#                     r-=1
+#                     while l<r and nums[l] == nums[l-1]:
+#                         l+=1
+#                 elif three_sum < 0:
+#                     l+=1
+#                 else:
+#                     r-=1
+#         return result
                 
